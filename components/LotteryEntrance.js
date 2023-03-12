d1042ea972620cf5f2ad1f17ee38ac17b4640819
JW <digitalhumanlife@gmail.com>
2023-03-12 18:01:31 +0900

    0312Home

---
+++ components/LotteryEntrance.js
@@ -0,0 +1,15 @@
+import { useWeb3Contract } from "react-moralis"
+
+import(useWeb3Contract)
+
+export default function LotteryEntrance() {
+    // const {runContractFuction: enterRaffle} = useWeb3Contract({
+    //     abi: //,
+    //     contractAddress: //,
+    //     functionName: //,
+    //     param:{},
+    //     msgValue:
+    // })
+
+    return <div>Hi from Lottery entrance!</div>
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
-import "@/styles/globals.css"
+import styles from "../styles/globals.css"
+
 import { MoralisProvider } from "react-moralis"
 
 export default function App({ Component, pageProps }) {
